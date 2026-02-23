0176531f61a5536bf81b1f878c7612999caf44da
Sabbir-75 <mdsabbirhossain9200@gmail.com>
2026-02-23 22:44:43 +0600

    Implement Box class with friend function and VolumeCalculator friend class

--- OOP/Lab-2/work-1/work1.cpp
+++ OOP/Lab-2/work-1/work1.cpp
@@ -15,22 +15,22 @@ public:
         height = hght;
     }
 
-    friend void display()
-    friend class 
+    friend void display(Box values);
+    friend class VolumeCalculator;
 };
-void display()
+void display(Box values)
 {
-    cout << "length : " << length << "\n";
-    cout << "width : " << width << "\n";
-    cout << "height : " << height << "\n";
-    cout << "==================================\n";
+    cout << "length : " << values.length << "\n";
+    cout << "width : " << values.width << "\n";
+    cout << "height : " << values.height << "\n";
+    cout << "----------------------------\n";
 };
 class VolumeCalculator
 {
 public:
-    float vocalc()
+    float vocalc(Box values)
     {
-        float volume = length * width * height;
+        float volume = values.length * values.width * values.height;
         return volume;
     }
 };
@@ -38,5 +38,14 @@ public:
 int main()
 {
     Box box1(12, 14, 16);
-    box1.display();
+    display(box1);
+
+    VolumeCalculator vlmcalc;
+    cout << "Volume For Box 1 : " << vlmcalc.vocalc(box1) << "\n\n";
+
+    Box box2(12.45, 14.55, 16.45);
+    display(box2);
+    cout << "Volume For Box 2 : " << vlmcalc.vocalc(box2) << "\n";
+
+    return 0;
 }
